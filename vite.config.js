97b0afc80dacb3c4c9ac7aa7f26f1c6b385df086
arnabjoy123 <arnab99joy@gmail.com>
2025-06-05 16:43:56 +0530

    sitemap changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import createPagesSitemap from "vite-plugin-pages-sitemap";
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createPagesSitemap({
+      hostname: "https://eventandus.com", // ✅ change this to your domain
+    }),
+  ],
   server: {
     host: true,
   },
