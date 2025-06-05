bf0e6c557268e54bea20597d80c60032dd95af83
arnabjoy123 <arnab99joy@gmail.com>
2025-06-05 16:48:27 +0530

    sdas

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import createPagesSitemap from "vite-plugin-pages-sitemap";
+import { createPagesSitemap } from "vite-plugin-pages-sitemap";
 // https://vite.dev/config/
 
 export default defineConfig({
